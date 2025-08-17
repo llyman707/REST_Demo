ce8d2294ddd6c8d1ea77db0d3f81c1042ddabf27
llyman707 <llyman707@gmail.com>
2025-08-17 14:17:13 -0400

    adding univerally unique identifiers to users

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -9,22 +10,22 @@ app.set('view engine', 'ejs')
 
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
-        id: 2,
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
-        id: 3,
+        id: uuid(),
         username: 'Sk8terBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
-        id: 4,
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -40,13 +41,13 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     res.redirect('/comments');
 })
 
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', {comment})
 })
 
