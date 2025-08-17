ee80325b3d0dca8f651f1eeffd46c4d5a2239f8c
llyman707 <llyman707@gmail.com>
2025-08-17 16:10:30 -0400

    setting up a destroy request

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -66,6 +66,12 @@ app.patch('/comments/:id', (req, res) => {
     const newCommentText = req.body.comment;
     foundComment.comment = newCommentText;
     res.redirect('/comments')
+})
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
 })
 
 app.get('/tacos', (req, res) => {
