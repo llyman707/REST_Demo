20a40c8d53b55ad947954b1f7bb6ed96c1291f03
llyman707 <llyman707@gmail.com>
2025-08-17 13:46:12 -0400

    Testing out express redirects

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol that is so funny!'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I like to go birdwatching with my dog'
+    },
+    {
+        username: 'Sk8terBoi',
+        comment: 'Plz delete your account, Todd'
+    },
+    {
+        username: 'onlysayswoof',
+        comment: 'woof woof woof'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+app.post('/comments', (req, res) => {
+    const { username, comment } = req.body;
+    comments.push({ username, comment })
+    res.redirect('/comments');
+})
+
+app.get('/tacos', (req, res) => {
+    res.send("Get /tacos response")
+});
+
+app.post('/tacos', (req, res) => {
+    const { meat, qty } = req.body;
+    res.send(`Ok, here are your ${qty} ${meat} tacos`)
+});
+
+app.listen(3000, () => {
+    console.log("On Port 3000!")
+})
+
+
